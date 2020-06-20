29c25ba063bf9463f594b62cbca0d363750f3c3e
hyeonjiseon <hyeonjiiseon@gmail.com>
2020-06-20 18:26:40 +0900

    copy blog
    https://junyoung-jamong.github.io/computer/vision/2019/02/07/Android-Tesseract-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0.html

--- readOCR/app/src/main/java/company/co/readocr/MainActivity.java
+++ readOCR/app/src/main/java/company/co/readocr/MainActivity.java
@@ -2,29 +2,160 @@ package company.co.readocr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.googlecode.tesseract.android.TessBaseAPI;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity{
+    TessBaseAPI tessBaseAPI;//Tesseract 사용을 위한 관련 클래스 객체를 생성해줘야 한다.
+    // TessBaseAPI 클래스 객체를 위한 참조변수를 정의하였다.
+
+    Button button;
+    ImageView imageView;
+    CameraSurfaceView surfaceView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        imageView = findViewById(R.id.imageView);
+        surfaceView = findViewById(R.id.surfaceView);
+        textView = findViewById(R.id.textView);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                capture();
+            }
+        });
+
+        //OnCreate 내부에서 TessBaseAPI 객체를 생성하고, Tesseract OCR에 적용할 언어를 지정한다.
+        // 언어 관련 파일은 사전에 assets 폴더나 외장 스토리지에 저장해둬야 이용할 수 있다.
+        tessBaseAPI = new TessBaseAPI();
+        String dir = getFilesDir() + "/tesseract";
+        if(checkLanguageFile(dir+"/tessdata"))
+            tessBaseAPI.init(dir, "eng");
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    //프로그램 처음 실행 시 checkLanguageFile() 함수를 통해 내부 디렉토리에 해당 파일이 존재하는 지 체크하고,
+    // 을 경우 createFiles() 함수에서 Assets 폴도 내 언어 데이터 파일을 읽어 지정된 경로에 파일을 복사한다.
+    boolean checkLanguageFile(String dir)
+    {
+        File file = new File(dir);
+        if(!file.exists() && file.mkdirs())
+            createFiles(dir);
+        else if(file.exists()){
+            String filePath = dir + "/eng.traineddata";
+            File langDataFile = new File(filePath);
+            if(!langDataFile.exists())
+                createFiles(dir);
+        }
+        return true;
+    }
+
+    private void createFiles(String dir)
+    {
+        AssetManager assetMgr = this.getAssets();
+
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+
+        try {
+            inputStream = assetMgr.open("eng.traineddata");
+
+            String destFile = dir + "/eng.traineddata";
+
+            outputStream = new FileOutputStream(destFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+            inputStream.close();
+            outputStream.flush();
+            outputStream.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //텍스트 인식 버튼 클릭 시 capture() 함수가 호출되는데, 여기서 surfaceView를 통해 이미지를 캡쳐하고,
+    // 해당 이미지를 imageView에 표시한다. 또한 동일한 이미지를 tesseract ocr 입력 데이터로 사용한다.
+    private void capture()
+    {
+        surfaceView.capture(new Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] bytes, Camera camera) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 8;
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                bitmap = GetRotatedBitmap(bitmap, 90);
+
+                imageView.setImageBitmap(bitmap);
+
+                button.setEnabled(false);
+                button.setText("텍스트 인식중...");
+                new AsyncTess().execute(bitmap);
+
+                camera.startPreview();
+            }
+        });
+    }
+
+    public synchronized static Bitmap GetRotatedBitmap(Bitmap bitmap, int degrees) {
+        if (degrees != 0 && bitmap != null) {
+            Matrix m = new Matrix();
+            m.setRotate(degrees, (float) bitmap.getWidth() / 2, (float) bitmap.getHeight() / 2);
+            try {
+                Bitmap b2 = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), m, true);
+                if (bitmap != b2) {
+                    bitmap = b2;
+                }
+            } catch (OutOfMemoryError ex) {
+                ex.printStackTrace();
+            }
+        }
+        return bitmap;
+    }
+
+    //tessBaseAPI 클래스 객체의 setImage 함수의 인자로 이미지를 설정하고
+    // getUTF8Text()를 통해 텍스트 추출 결과를 얻을 수 있다.
+    //실질적으로는 tessBaseAPI.setImage(); tessBaseAPI.getUTF8Text();
+    // 두 개의 함수 호출로 이미지로부터 텍스트를 인식/추출할 수 있다.
+    private class AsyncTess extends AsyncTask<Bitmap, Integer, String> {
+        @Override
+        protected String doInBackground(Bitmap... mRelativeParams) {
+            tessBaseAPI.setImage(mRelativeParams[0]);
+            return tessBaseAPI.getUTF8Text();
+        }
+
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+            Toast.makeText(MainActivity.this, ""+result, Toast.LENGTH_LONG).show();
+
+            button.setEnabled(true);
+            button.setText("텍스트 인식");
+        }
+    }
 }
